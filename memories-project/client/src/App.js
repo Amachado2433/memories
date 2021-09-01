a6b5eab86dc28e6e759809aa0682a773b1254382
Amachado2433 <31517633+Amachado2433@users.noreply.github.com>
2021-09-01 13:24:07 -0400

    Set up Redux Store

--- memories-project/client/src/App.js
+++ memories-project/client/src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+import { getPosts } from "./actions/posts";
 
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
@@ -8,6 +11,11 @@ import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container maxwidth="lg">
@@ -26,7 +34,7 @@ const App = () => {
         <Container>
           <Grid
             container
-            justify="space-between"
+            justifyContent="space-between"
             alignItems="stretch"
             spacing={3}
           >
